63070f8d93d4520ccea85f1fe58dfa1afdbe7308
Priscilla Rodriguez <priss@prisskreative.com>
2015-08-24 14:02:45 -0400

    Modified spotironify.js and css

--- app/assets/javascripts/spotironify.js
+++ app/assets/javascripts/spotironify.js
@@ -18,23 +18,59 @@ function spotifySearch () {
 
 
   function searchSong(song) {
-    //console.log(characters.artists.items)
-    song.tracks.items.forEach(function append (chr) {
-
-      $('.js-songs-list').append(chr.name );
-     
-       console.debug(chr.name);
-    });
+    var firstSong = song.tracks.items[0];
+    $('.title').text(firstSong.name);        
   }
+
+  function artist(art) {
+    var firstSongArtist = art.tracks.items[0].artists[0];
+    $('.author').text(firstSongArtist.name);   
+  }
+
+  function trackImage(img) {
+    var firsttrackImage = img.tracks.items[0].album.images[0];
+    $('.coverimg').prop("src", firsttrackImage.url); // change to this url   
+  }
+
+  function playSong(cc) {
+  	var firstSongPlay = cc.tracks.items[0];
+    $('#audio').prop("src", firstSongPlay.preview_url);
+
+    $('.btn-play').click(function(){
+    	if (!$('.btn-play').hasClass('playing')){
+    	    $(this).addClass('playing');
+    	    $('#audio').trigger('play');
+        }
+    	else{
+    		$(this).addClass('disabled').removeClass('playing');
+    	    $('#audio').trigger('pause');
+    	}
+    }); 
+
+    }
+
+ 
 
   function handleError (err1, err2, err3) {
     console.error('OH NO!!', err1, err2, err3);
   }
 
   request.done(searchSong);
+  request.done(artist);
+  request.done(trackImage);
+  request.done(playSong);
   request.fail(handleError);
+  }
+
+function printTime () {
+    $('.js-progress-bar').prop("value", audio.currentTime);
 }
-
-
  
-$('.search').on('click', spotifySearch);
+$('.js-search-button').on('click', spotifySearch);
+$('#audio').on('timeupdate', printTime);
+
+
+$('.js-modal').modal();
+
+
+
